e9ac8253db9036157e6b0fc36a1d441478a1bc49
xww <xww>
2024-01-11 16:42:33 +0800

    use TABLE COLUMNS

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"os"
 	"path"
+	"regexp"
 	"strings"
 	"text/template"
 )
@@ -21,7 +22,7 @@ package {{.Package}}
 
 // {{.StructName}} struct is a row record of the {{.TableName}} table in the {{.DatabaseName}} database
 type {{.StructName}} struct {
-    {{range .Columns}}{{.CamelCase | title}} {{.Type}} ` + "`gorm:\"{{.GormTag}}\" " + "json:\"{{.Field}}\"`" + `{{if .Comment}}// {{.Comment}}{{end}} 
+    {{range .Columns}}{{.CamelCase | title}} {{.Type}} ` + "`gorm:\"{{.GormTag}}\" " + "json:\"{{.JsonTag}}\"`" + `{{if .Comment}}// {{.Comment}}{{end}} 
     {{end}}
 }
 
@@ -90,10 +91,22 @@ func getTables(db *sql.DB, dataBase string) (tables []string, err error) {
 	return
 }
 
+type Column struct {
+	Field   string
+	Type    string
+	Null    string
+	Key     string
+	Default sql.NullString // 使用 sql.NullString 表示可能为 NULL 的字段
+	Extra   string
+	Comment string
+	GormTag string
+}
+
 func generateStruct(db *sql.DB, tableName, database, dir, packageName string) (err error) {
 	log.Printf(`gen %s`, tableName)
 
-	rows, err := db.Query(fmt.Sprintf("DESCRIBE `%s`", tableName))
+	rows, err := db.Query(`SELECT COLUMN_NAME,DATA_TYPE,IS_NULLABLE,COLUMN_KEY,COLUMN_DEFAULT,EXTRA,COLUMN_COMMENT FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA =? AND TABLE_NAME =?`, database, tableName)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -101,16 +114,16 @@ func generateStruct(db *sql.DB, tableName, database, dir, packageName string) (e
 	var columns []Column
 	for rows.Next() {
 		var column Column
-		if err := rows.Scan(&column.Field, &column.Type, &column.Null, &column.Key, &column.Default, &column.Extra); err != nil {
+		if err := rows.Scan(&column.Field, &column.Type, &column.Null, &column.Key, &column.Default, &column.Extra, &column.Comment); err != nil {
 			log.Fatal(err)
 		}
 		columns = append(columns, column)
 	}
 
 	rows.Close()
-	for i, _ := range columns {
-		columns[i].Comment = getColumnComment(db, tableName, database, columns[i].Field)
-	}
+	//for i, _ := range columns {
+	//	columns[i].Comment = getColumnComment(db, tableName, database, columns[i].Field)
+	//}
 	structFields := make([]StructField, 0, len(columns))
 	for _, column := range columns {
 		structFields = append(structFields, column.GetStructField())
@@ -129,25 +142,6 @@ func generateStruct(db *sql.DB, tableName, database, dir, packageName string) (e
 	return
 }
 
-func generateFile(tableName, packageName, dir string, columns []Column) {
-	s := generateGoStruct(tableName, packageName, columns)
-	fileName := tableName + ".go"
-	if dir != "" {
-		if _, err := os.Stat(dir); os.IsNotExist(err) {
-			if err := os.MkdirAll(dir, 0755); err != nil {
-				log.Fatalf("Error creating output directory: %v", err)
-			}
-		}
-		fileName = path.Join(dir, fileName)
-	}
-
-	file, err := os.Create(fileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	file.WriteString(s)
-}
 func generateFileWithTmpl(tb Struct) {
 	fileName := tb.TableName + ".go"
 	if tb.Dir != "" {
@@ -198,17 +192,6 @@ type Struct struct {
 	Dir          string
 }
 
-type Column struct {
-	Field   string
-	Type    string
-	Null    string
-	Key     string
-	Default sql.NullString // 使用 sql.NullString 表示可能为 NULL 的字段
-	Extra   string
-	Comment string
-	GormTag string
-}
-
 func (c *Column) GetStructField() StructField {
 	sf := StructField{
 		CamelCase: toCamelCase(c.Field),
@@ -216,6 +199,7 @@ func (c *Column) GetStructField() StructField {
 		Type:      mysqlTypeToGoType(c.Type, c.Null),
 		Comment:   c.Comment,
 		GormTag:   c.getGormTag(),
+		JsonTag:   CamelCaseToUnderscore(c.Field),
 	}
 	return sf
 }
@@ -226,6 +210,7 @@ type StructField struct {
 	Type      string
 	Comment   string
 	GormTag   string
+	JsonTag   string
 }
 
 // getGormTag 用于生成gorm tag
@@ -240,7 +225,7 @@ func (c *Column) getGormTag() string {
 	if c.Default.Valid && c.Default.String != "" {
 		tag += ";default:" + c.Default.String
 	}
-	if c.Null == "NO" {
+	if c.Null == "NO" && c.Key != "PRI" {
 		tag += ";NOT NULL"
 	}
 
@@ -322,4 +307,23 @@ func toCamelCase(s string) string {
 		parts[i] = strings.Title(parts[i])
 	}
 	return strings.Join(parts, "")
+}
+
+// CamelCaseToUnderscore 将驼峰命名的字符串转换为下划线命名
+func CamelCaseToUnderscore(s string) string {
+	var words []string
+
+	// 使用正则表达式将字符串拆分为单词
+	re := regexp.MustCompile("[a-z0-9]+|[A-Z][a-z0-9]*")
+	matches := re.FindAllString(s, -1)
+
+	// 将每个单词转换为小写，并添加到切片中
+	for _, match := range matches {
+		words = append(words, strings.ToLower(match))
+	}
+
+	// 将切片中的单词用下划线连接起来
+	result := strings.Join(words, "_")
+
+	return result
 }
